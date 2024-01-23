dfa9917441c7fab76b352a8470ccab6a5285e9d6
kavitha <vkavitha434@gmail.com>
2024-01-23 22:25:56 +0530

    Added petid testing operations

---
+++ src/test/java/com/simplilearn/restassured_Assignment_006/LogoutRequestTest.java
@@ -0,0 +1,27 @@
+package com.simplilearn.restassured_Assignment_006;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import static org.hamcrest.Matchers.*;
+public class LogoutRequestTest {
+	
+
+    @Test(description="Test with logout scemario")
+    public void testLogoutEndpoint() {
+        
+
+       
+        RestAssured.given()
+        .baseUri("https://petstore.swagger.io/v2/user")
+            .when()
+                .get("/logout")
+            .then()
+                .assertThat()
+                .statusCode(200)  // Validate response status code
+                .body("code", equalTo(200))  // Validate 'code' field in the response
+                .body("message", equalTo("ok"));  // Validate 'message' field in the response
+    }
+
+}
